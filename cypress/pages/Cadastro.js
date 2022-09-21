f091195df44a037d183c780fb3b755e8e28635b3
Rogerio <rogerioopaiva36@gmail.com>
2022-09-21 02:37:57 -0300

    Finalizacao do cenarios e geracao de relatorio com screenshot em cada passo do teste

--- cypress/pages/Cadastro.js
+++ cypress/pages/Cadastro.js
@@ -6,14 +6,21 @@ class Cadastro {
 
     navegarParaPagina() {
         cy.visit('/')
+        cy.screenshot({
+            capture: 'runner',
+          })
     }
 
     visualizarPagina() {
         cy.get('body').should('be.visible')
+        cy.screenshot({
+            capture: 'runner',
+          })
     }
 
     encontrarTituloDaPagina() {
         cy.get('.col-12 h1').first().should('have.text', 'Formulário')
+        cy.screenshot({capture: 'runner'})
     }
 
     cadastrarComNomeInvalido() {
@@ -21,10 +28,12 @@ class Cadastro {
         cy.get('input[name="email"]').type(usuario.email)
         cy.get('input[name="password"]').type(usuario.senha)
         cy.get('form button[type="submit"]').click();
+        cy.screenshot({capture: 'runner'})
     }   
 
     validarMensagemNomeInvalido() {
         cy.get('.text-danger').contains('Insira um Nome e Sobrenome válido.')
+        cy.screenshot({capture: 'runner'})
     }
 
     //Email invalido
@@ -32,6 +41,7 @@ class Cadastro {
         cy.get('input[name="name"]').type(usuario.nomeCompleto)
         cy.get('input[name="email"]').type(usuario.emailInvalido)
         cy.get('input[name="password"]').type(usuario.senha)
+        cy.screenshot({capture: 'runner'})
     } 
     
     validarMensagemEmailInvalido() {
@@ -39,6 +49,7 @@ class Cadastro {
             cy.get('input[name="email"]').then(($input) => {
                 expect($input[0].validationMessage).to.eq('"." está sendo usado em uma posição incorreta em ".test.dev".')
             })
+            cy.screenshot({capture: 'runner'})
         }
 
         //Senha invalida
@@ -48,10 +59,12 @@ class Cadastro {
             cy.get('input[name="email"]').type(usuario.email)
             cy.get('input[name="password"]').type(usuario.senhaInvalida)
             cy.get('form button[type="submit"]').click();
+            cy.screenshot({capture: 'runner'})
         }
 
         validarMensagemSenhaInvalida() {
             cy.get('.text-danger').contains('O campo Password deve ter no minimo 8 caracteres.')
+            cy.screenshot({capture: 'runner'})
         }
 
         //Instrução 2.1
@@ -61,20 +74,24 @@ class Cadastro {
             cy.get('input[name="email"]').type(usuario.email)
             cy.get('input[name="password"]').type(usuario.senha)
             cy.get('form button[type="submit"]').click();
+            cy.screenshot({capture: 'runner'})
         }
 
         ValidarMensagemCadastroComSucesso() {
             cy.get('.alert-success').contains('Usuário cadastrado com sucesso.')
+            cy.screenshot({capture: 'runner'})
         }
 
          //Instrução 3
         validarTituloDaLista() {
             cy.get('.bg-gradient h1').last().should('have.text', 'Usuários cadastrados')
+            cy.screenshot({capture: 'runner'})
         }
 
         validarDadoIgualAoCadastrado() {
             cy.get('tr').should('contain', `${usuario.nomeCompleto}`)
             cy.get('tr').should('contain', `${usuario.email}`)
+            cy.screenshot({capture: 'runner'})
         }
 
         //Instrução 4
@@ -90,6 +107,7 @@ class Cadastro {
                 return false;
             });
             cy.get('button[form^=edit]').last().contains('Salvar').click();
+            cy.screenshot({capture: 'runner'})
         }
 
 
@@ -105,12 +123,14 @@ class Cadastro {
                 return false;
             });
             cy.get('button[form^=edit]').last().contains('Salvar').click();
+            cy.screenshot({capture: 'runner'})
         }
         
         validarMensagemEmailAtualizadoInvalido() {
             cy.get('input[id^="e_email"]').last().then(($input) => {
                 expect($input[0].validationMessage).to.eq('"." está sendo usado em uma posição incorreta em ".test.dev".')
             })
+            cy.screenshot({capture: 'runner'})
         }
 
         //Instrução 4.1
@@ -128,16 +148,19 @@ class Cadastro {
                 return false;
             });
             cy.get('button[form^=edit]').last().contains('Salvar').click();
+            cy.screenshot({capture: 'runner'})
         }
 
         validarMensagemAtualizadoComSucesso() {
             cy.get('.alert-success').contains('Usuário salvo com sucesso.')
+            cy.screenshot({capture: 'runner'})
         }
 
         //Instrução 5
         verificarDadoAtualizado() {
             cy.get('tr').contains(usuario.nomeCompleto)
             cy.get('tr').contains(usuario.email)
+            cy.screenshot({capture: 'runner'})
         }
 
         //Instrução 6
@@ -145,15 +168,18 @@ class Cadastro {
             cy.get('.btn.btn-danger.dropdown-toggle').last().click()
             cy.get('.dropdown-menu.show').contains('Excluir').click()
             cy.get('button[type="submit"]').contains('Excluir').click({force: true})
+            cy.screenshot({capture: 'runner'})
         }
 
         validarMesagemUsuarioRemovidoComSucesso() {
             cy.get('.alert-success').contains('Usuário removido com sucesso.')
+            cy.screenshot({capture: 'runner'})
         }
 
         //Instrução 7
         verificarCadastroNaoExiste() {
             cy.get(usuario.nomeCompleto).should('not.exist')
+            cy.screenshot({capture: 'runner'})
         }
 
 }
